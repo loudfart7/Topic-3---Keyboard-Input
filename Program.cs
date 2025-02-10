42b345958718665df4a92f6688d00d0936b73a16
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-02-10 14:03:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml.Linq;
+
+namespace Topic_3___Keyboard_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Joel
+            //Greetings
+            string name = "You";
+            string computer = "Computer";
+            string age;
+            string year;
+
+            Console.WriteLine($"{computer}:");
+            Console.WriteLine("Whats your name?");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{name}:");
+            name = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{computer}:");
+            Console.WriteLine($"Hello, {name}! How old are you..?");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{name}:");
+            age = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{computer}:");
+            Console.WriteLine($"{age}...");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{computer}:");
+            Console.WriteLine($"You're old.. Much older than I am... Could you maybe give me a name?");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{name}:");
+            computer = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{computer}:");
+            Console.WriteLine($"{computer}? Okay... Well what year is it?");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{name}:");
+            year = Console.ReadLine();
+
+        }
+    }
+}
